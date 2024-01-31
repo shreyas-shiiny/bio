e615346802752395cd2c5a4f100040151a7f5656
shreyas shiiny <130795920+shreyas-shiiny@users.noreply.github.com>
2024-01-31 17:32:26 +0530

    Update EducationPage.js

--- src/Components/EducationPage.js
+++ src/Components/EducationPage.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Col } from "react-bootstrap";
-import dipphoto from "../Components/Images/2015-10-28_17.47.30.jpg"
-import beone from "../Components/Images/DSC_0198.JPG"
-import betwo from "../Components/Images/WhatsApp Image 2024-01-30 at 6.41.01 PM.jpeg"
-import meone from "../Components/Images/WhatsApp Image 2024-01-30 at 7.37.39 PM (1).jpeg"
-import metwo from "../Components/Images/WhatsApp Image 2024-01-30 at 7.37.39 PM.jpeg"
-import methree from "../Components/Images/WhatsApp Image 2024-01-30 at 7.37.40 PM.jpeg"
-
+import dipphoto from "../Components/Images/2015-10-28_17.47.30.jpg";
+import beone from "../Components/Images/DSC_0198.JPG";
+import betwo from "../Components/Images/WhatsApp Image 2024-01-30 at 6.41.01 PM.jpeg";
+import meone from "../Components/Images/WhatsApp Image 2024-01-30 at 7.37.39 PM (1).jpeg";
+import metwo from "../Components/Images/WhatsApp Image 2024-01-30 at 7.37.39 PM.jpeg";
+import methree from "../Components/Images/WhatsApp Image 2024-01-30 at 7.37.40 PM.jpeg";
 
 const EducationPage = () => {
   const [showDiplomaPhotos, setShowDiplomaPhotos] = useState(false);
@@ -15,13 +14,13 @@ const EducationPage = () => {
 
   const handleViewPhotosClick = (field) => {
     switch (field) {
-      case 'diploma':
+      case "diploma":
         setShowDiplomaPhotos(!showDiplomaPhotos);
         break;
-      case 'be':
+      case "be":
         setShowBEPhotos(!showBEPhotos);
         break;
-      case 'mtech':
+      case "mtech":
         setShowMTechPhotos(!showMTechPhotos);
         break;
       default:
@@ -29,98 +28,112 @@ const EducationPage = () => {
     }
   };
 
-  
-
   return (
     <div>
-
       <div className="education-box">
         <h3>Diploma</h3>
         {showDiplomaPhotos && (
           <div>
             <h3>Completed Diploma in Electronics and Communication (E&C)</h3>
             <Col md={5} style={{ paddingBottom: 20 }}>
-    <img
-      src={dipphoto}
-      alt="home pic"
-      className="img-fluid"
-      style={{ maxHeight: "450px" }}
-    />
-    </Col>
+              <img
+                src={dipphoto}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
           </div>
         )}
-        <button onClick={() => handleViewPhotosClick('diploma')}>View Info </button><br/><br/>
-        
+        <button onClick={() => handleViewPhotosClick("diploma")}>
+          {showDiplomaPhotos ? "Close Info" : "View Info"}
+        </button>
+        <br />
+        <br />
       </div>
 
       <div className="education-box">
         <h3>B.E(UG)</h3>
         {showBEPhotos && (
           <div>
-            <h2>With many surgical efforts and hardworks I have Successfully Completed my Bachelor of Engineering(Under Graducation) in Electronics and Communication engineering(E&C)under VTU: Visvesvaraya Technological University</h2>
+            <h2>
+              With many surgical efforts and hardworks I have Successfully
+              Completed my Bachelor of Engineering(Under Graduation) in
+              Electronics and Communication engineering(E&C)under VTU:
+              Visvesvaraya Technological University
+            </h2>
             <Col md={5} style={{ paddingBottom: 20 }}>
-    <img
-      src={beone}
-      alt="home pic"
-      className="img-fluid"
-      style={{ maxHeight: "450px",
-     }}
-    />
-    </Col>
-    <Col md={5} style={{ paddingBottom: 20 }}>
-    <img
-      src={betwo}
-      alt="home pic"
-      className="img-fluid"
-      style={{ maxHeight: "450px", 
-    }}
-    />
-    </Col>
-    
+              <img
+                src={beone}
+                alt="home pic"
+                className="img-fluid"
+                style={{
+                  maxHeight: "450px",
+                }}
+              />
+            </Col>
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={betwo}
+                alt="home pic"
+                className="img-fluid"
+                style={{
+                  maxHeight: "450px",
+                }}
+              />
+            </Col>
           </div>
         )}
-        <button onClick={() => handleViewPhotosClick('be')}>View Info</button> <br/><br/>
-       
+        <button onClick={() => handleViewPhotosClick("be")}>
+          {showBEPhotos ? "Close Info" : "View Info"}
+        </button>{" "}
+        <br />
+        <br />
       </div>
 
       <div className="education-box">
         <h3>M.Tech(PG)</h3>
         {showMTechPhotos && (
           <div>
-            <h1>Completed Masters of Technology(Post Graducation) in Electronics and Communication engineering(E&C)</h1>
+            <h1>
+              Completed Masters of Technology(Post Graduation) in Electronics
+              and Communication engineering(E&C)
+            </h1>
             <Col md={5} style={{ paddingBottom: 20 }}>
-    <img
-      src={meone}
-      alt="home pic"
-      className="img-fluid"
-      style={{ maxHeight: "450px" }}
-    />
-    </Col>
-    <Col md={5} style={{ paddingBottom: 20 }}>
-    <img
-      src={metwo}
-      alt="home pic"
-      className="img-fluid"
-      style={{ maxHeight: "450px" }}
-    />
-    </Col>
-    <Col md={5} style={{ paddingBottom: 20 }}>
-    <img
-      src={methree}
-      alt="home pic"
-      className="img-fluid"
-      style={{ maxHeight: "450px" }}
-    />
-    </Col>
+              <img
+                src={meone}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={metwo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={methree}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
           </div>
         )}
-        <button onClick={() => handleViewPhotosClick('mtech')}>View Info</button><br/><br/>
-       
-        
-        
+        <button onClick={() => handleViewPhotosClick("mtech")}>
+          {showMTechPhotos ? "Close Info" : "View Info"}
+        </button>
+        <br />
+        <br />
       </div>
     </div>
-  );
-};
-
-export default EducationPage;
+    );
+  };
+  
+  export default EducationPage;
+  
