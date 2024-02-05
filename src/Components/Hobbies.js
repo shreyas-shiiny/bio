8561e1f4b006333ee7f957fdd45fa30e2742295d
shreyas shiiny <130795920+shreyas-shiiny@users.noreply.github.com>
2024-02-05 11:54:18 +0530

    Update Hobbies.js

--- src/Components/Hobbies.js
+++ src/Components/Hobbies.js
@@ -59,7 +59,8 @@ const Hobbies = () => {
   return (
     <div>
       <h1>I have a hidden talent for writing ಕನ್ನಡ ಶಾಯರಿ</h1>
-<h1> please wait ಶಾಯರಿ 1,6,7,8 will updated soon  </h1>
+    <br/><br/><br/>
+<h3> please wait ಶಾಯರಿ 1,6,7,8 will updated soon  </h3>
     <h3> Thank-you</h3>
       <div>
         <h3>ಶಾಯರಿ 1</h3>
